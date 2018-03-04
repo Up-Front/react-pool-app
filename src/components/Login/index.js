dd2ef7bc230ddfbe38745f63f2a5e1ae55917a2d
Maurits Ridder <maurits.ridder@gmail.com>
2018-03-04 19:27:32 +0100

    Adds login button styling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,7 +5,8 @@ import {connect} from 'react-redux'
 import {firebaseConnect, isLoaded, isEmpty} from 'react-redux-firebase'
 import Lottie from 'react-lottie';
 import * as animationData from "../../pool";
-import {get} from 'lodash'
+import styled from 'styled-components'
+import {FadeIn} from 'animate-css-styled-components';
 
 const defaultOptions = {
   loop: false,
@@ -13,28 +14,56 @@ const defaultOptions = {
   animationData: animationData
 };
 
+const Login = styled.div`
+  display:flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items:center;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  padding: 12px 12px;
+  margin:10px;
+  font-size: 120%;
+  cursor: pointer;
+  border: 0 none;
+  border-radius: 4px;
+  color: black;
+  background: rgba(255,255,255,0.5);
+
+  :hover {
+    background-color: #517fa4;
+    transition: all 150ms linear;
+  }
+`;
+
 export const LoginPage = ({firebase, auth}) => {
   return (
     <div>
-      {isLoaded(auth) && isEmpty(auth)
-        ? <button
-          onClick={() => firebase.login({provider: 'google', type: 'popup'})}
-        >Login With Google
-        </button>
-        :
-        <button
-          onClick={() => firebase.logout()}
-        >Log out
-        </button>
-      }
-      <div>
+      <Login>
+        {isLoaded(auth) && isEmpty(auth)
+          ? <Button
+            onClick={() => firebase.login({provider: 'google', type: 'popup'})}
+          >
+            Log in
+          </Button>
+          :
+          <Button
+            onClick={() => firebase.logout()}
+          >
+            Log out
+          </Button>
+        }
         {
           isLoaded(auth) && isEmpty(auth)
-            ? <span>You are not logged in</span>
-            : <span>Logged in as {auth.displayName}</span>
+            ? null
+            : <FadeIn delay='0.5s'><span>Welcome, </span><strong>{auth.displayName}</strong></FadeIn>
         }
-      </div>
-      <Lottie options={defaultOptions} />
+
+      </Login>
+      <Lottie options={defaultOptions}/>
     </div>
   )
 };
