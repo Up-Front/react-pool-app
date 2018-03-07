e2ca625119c1aa9b94e51d98e6e600ca4929ed47
Steven Straatemans <steven@straatemans.nl>
2018-03-07 22:49:01 +0100

    redirect if not loggedin

--- src/components/shared/components/Authorization/index.js
+++ src/components/shared/components/Authorization/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 
 const Authorization = (Component) => {
-    const BaseAuth = ({ auth }) => {
+    const BaseAuth = ({ auth, ...props }) => {
+        console.log(auth);
         if (isLoaded(auth) && !isEmpty(auth)) {
-            return <Component auth={auth} />
-        } else {
-            return 'You are not allowed';
+            return <Component auth={auth} {...props} />
+        } else if (isLoaded(auth) && isEmpty(auth)) {
+            props.history.push('/login');
+            return null;
         }
+        return '';
     }
     const enhance = compose(
         firebaseConnect((props) => [
