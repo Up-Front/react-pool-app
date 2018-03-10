181e33d481f9df326c61e5cadb27d08f6b6ad822
Steven Straatemans <steven@straatemans.nl>
2018-03-10 09:07:51 +0100

    refactor match results

--- src/actions/matches.js
+++ src/actions/matches.js
@@ -22,13 +22,50 @@ export const addMatch = users => {
   });
 };
 
+// set the winner of the match
+// also set the winner/loser with the competitor matches
 export const declareWinner = (matchId, match, winner, declarer) => {
+  const ref = database.ref('/');
+  let updateData = {};
   const winners = Object.assign({}, match.winners, {
     [declarer.uid]: winner.uid
   });
   match = Object.assign({}, match, { winners });
-  const { competitors, ...update } = setMatchStatus(matchId, match);
-  return database.ref(`/matches/${matchId}`).update(update);
+  match = setMatchStatus(matchId, match);
+
+  updateData[`matches/${matchId}/winners`] = match.winners;
+  updateData[`matches/${matchId}/isContested`] = match.isContested;
+
+  if (match.winner) {
+    updateData = Object.assign({}, updateData, updateWinnerData(matchId, match));
+  }
+
+  return ref.update(updateData, function(error) {
+    if (error) {
+      console.log('Error updating data:', error);
+    }
+  });
+};
+
+const updateWinnerData = (matchId, match) => {
+  let updateData = {};
+  updateData[`matches/${matchId}/winner`] = match.winner;
+  updateData[`matches/${matchId}/finishedAt`] = match.finishedAt;
+
+  Object.values(match.competitors).map(competitor => {
+    let result;
+    let streak = competitor.streak || '';
+    if (match.winner === competitor.uid) {
+      result = 'W';
+    } else {
+      result = 'L';
+    }
+
+    updateData[`users/${competitor.uid}/matches/${matchId}`] = result;
+    updateData[`users/${competitor.uid}/streak`] = competitor.streak = streak + result;
+  });
+
+  return updateData;
 };
 
 //the match needs to be removed from /matches
@@ -53,9 +90,7 @@ export const setMatchStatus = (matchId, match) => {
   let isContested = false;
 
   // if there are not the same amount of winners as competitors than there is nothing to do
-  if (
-    Object.keys(match.winners).length !== Object.keys(match.competitors).length
-  ) {
+  if (Object.keys(match.winners).length !== Object.keys(match.competitors).length) {
     return match;
   }
   Object.values(match.winners).forEach(value => {
