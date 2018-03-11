7c701d5c34b07df27b6b5aa91b25b948c1ddfd7e
Steven Straatemans <steven@straatemans.nl>
2018-03-11 21:27:35 +0100

    put create match in modal

--- src/components/CreateMatch/index.js
+++ src/components/CreateMatch/index.js
@@ -3,112 +3,145 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 import { addMatch } from './../../actions/matches';
+import Modal from './../shared/components/Modal';
 import List from './components/List';
 import { User } from './../UserList/';
-import { SelectOpponent } from './styles';
+import { SelectOpponent, FloatButton } from './styles';
 
 class CreateMatch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            search: '',
-            filteredUsers: [],
-            selectedOpponent: null,
-            matchCreated: false,
-        };
-        this.selectOpponent = this.selectOpponent.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      filteredUsers: [],
+      selectedOpponent: null,
+      matchCreated: false,
+      openModal: true
+    };
+    this.selectOpponent = this.selectOpponent.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
 
-    filterUsers = filterTerm => {
-        if (!filterTerm) {
-            return [];
-        }
-        return this.props.users
-            .filter((user) => (user.value.displayName.toLowerCase().indexOf(filterTerm.toLowerCase()) > -1
-                &&
-                user.key !== this.props.auth.uid  // do not select the auth user
-            ))
+  filterUsers = filterTerm => {
+    if (!filterTerm) {
+      return [];
     }
+    return this.props.users.filter(
+      user =>
+        user.value.displayName.toLowerCase().indexOf(filterTerm.toLowerCase()) >
+          -1 && user.key !== this.props.auth.uid // do not select the auth user
+    );
+  };
 
-    handleChange = event => {
-        const search = event.target.value;
-        const users = this.filterUsers(search);
+  handleChange = event => {
+    const search = event.target.value;
+    const users = this.filterUsers(search);
+    this.setState({
+      search,
+      filteredUsers: users
+    });
+  };
+
+  selectOpponent = user => {
+    this.setState({
+      selectedOpponent: user,
+      filteredUsers: []
+    });
+  };
+
+  showOpponent = () => {
+    if (this.state.selectedOpponent) {
+      return <div>{this.state.selectedOpponent.displayName}</div>;
+    }
+  };
+
+  showUser = () => {
+    if (isLoaded(this.props.auth) || !isEmpty(this.props.auth)) {
+      return <div>{this.props.auth.displayName}</div>;
+    }
+  };
+
+  createMatch = () => {
+    addMatch([this.props.auth.uid, this.state.selectedOpponent.uid]).then(
+      () => {
         this.setState({
-            search,
-            filteredUsers: users,
+          matchCreated: true
         });
-    }
+        console.log('match created');
+      }
+    );
+  };
 
-    selectOpponent = user => {
-        this.setState({
-            selectedOpponent: user,
-            filteredUsers: [],
-        });
-    }
+  handleOpenModal = () => {
+    this.setState({
+      openModal: true
+    });
+  };
 
-    showOpponent = () => {
-        if (this.state.selectedOpponent) {
-            return (
-                <div>
-                    {this.state.selectedOpponent.displayName}
-                </div>
-            );
+  handleCloseModal = () => {
+    this.setState({
+      openModal: false
+    });
+  };
+
+  render() {
+    return [
+      <FloatButton onClick={this.handleOpenModal} key="FloatButton">
+        +
+      </FloatButton>,
+      <Modal
+        open={this.state.openModal ? 'open' : false}
+        closeModal={this.handleCloseModal}
+        key="CreateMatch"
+        footer={
+          <button
+            onClick={this.createMatch}
+            disabled={!this.state.selectedOpponent}
+          >
+            create show down
+          </button>
         }
-    }
+      >
+        <div>
+          {this.showUser()}
+          {this.showOpponent()}
 
-    showUser = () => {
-        if (isLoaded(this.props.auth) || !isEmpty(this.props.auth)) {
-            return (
-                <div>
-                    {this.props.auth.displayName}
-                </div>
-            );
-        }
-    }
-
-    createMatch = () => {
-        addMatch([this.props.auth.uid, this.state.selectedOpponent.uid])
-            .then(() => {
-                this.setState({
-                    matchCreated: true,
-                });
-                console.log('match created');
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                {this.showUser()}
-                {this.showOpponent()}
-
-                <SelectOpponent matchCreated={this.state.matchCreated}>
-                    <input onChange={this.handleChange} value={this.state.search} type="text" />
-                    <List>
-                        {this.state.filteredUsers && this.state.filteredUsers.map(
-                            ({ key, value: user }) => (<User handleClick={this.selectOpponent} online={this.props.presence[key]} key={key} uid={key} {...user} />)
-                        )}
-                    </List>
-
-                    <button onClick={this.createMatch} disabled={!this.state.selectedOpponent}>create show down</button>
-                </SelectOpponent>
-            </div>
-        );
-    }
-};
+          <SelectOpponent matchCreated={this.state.matchCreated}>
+            <input
+              onChange={this.handleChange}
+              value={this.state.search}
+              type="text"
+            />
+            <List>
+              {this.state.filteredUsers &&
+                this.state.filteredUsers.map(({ key, value: user }) => (
+                  <User
+                    handleClick={this.selectOpponent}
+                    online={this.props.presence[key]}
+                    key={key}
+                    uid={key}
+                    {...user}
+                  />
+                ))}
+            </List>
+          </SelectOpponent>
+        </div>
+      </Modal>
+    ];
+  }
+}
 
 export default compose(
-    firebaseConnect(props => [
-        { path: 'presence' },
-        { path: 'users' },
-        { path: 'auth' },
-        { path: 'matches' },
-
-    ]),
-    connect((state, props) => ({
-        presence: state.firebase.data.presence || {},
-        users: state.firebase.ordered.users,
-        auth: state.firebase.auth,
-        matches: state.firebase.ordered.matches
-    }))
+  firebaseConnect(props => [
+    { path: 'presence' },
+    { path: 'users' },
+    { path: 'auth' },
+    { path: 'matches' }
+  ]),
+  connect((state, props) => ({
+    presence: state.firebase.data.presence || {},
+    users: state.firebase.ordered.users,
+    auth: state.firebase.auth,
+    matches: state.firebase.ordered.matches
+  }))
 )(CreateMatch);
--- src/components/CreateMatch/styles.js
+++ src/components/CreateMatch/styles.js
@@ -1,6 +1,22 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const SelectOpponent = styled.section`
-   display: ${props => props.matchCreated ? 'none' : 'block'};
+  display: ${props => (props.matchCreated ? 'none' : 'block')};
+`;
 
+export const FloatButton = styled.button`
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 0;
+
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 24px;
+
+  z-index: 998;
 `;
