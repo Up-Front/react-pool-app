e139d7846a4bb10c6fda715dfdbaf109c4216e2c
Steven Straatemans <steven@straatemans.nl>
2018-03-11 21:27:35 +0100

    put create match in modal

--- src/components/CreateMatch/components/List/index.js
+++ src/components/CreateMatch/components/List/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const List = props => {
-    return (
-        <div>
-            <h4>choose your victim!</h4>
-            <ul>
-                {props.children}
-            </ul>
-        </div >
-    );
+  return (
+    <div>
+      <h4>choose your victim!</h4>
+      <ul>{props.children}</ul>
+    </div>
+  );
 };
 
 export default List;
