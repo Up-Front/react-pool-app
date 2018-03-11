1945ff4f643a3f8f70c53281ea7a42caf25d8ebe
Steven Straatemans <steven@straatemans.nl>
2018-03-11 22:54:13 +0100

    modal tests

--- src/components/shared/components/Modal/index.js
+++ src/components/shared/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   ModalWrapper,
@@ -8,21 +8,28 @@ import {
   ModalFooter
 } from './styles';
 
-const Modal = props => {
-  const handleRemoveModal = () => {
-    console.log('remove modal');
-    props.closeModal();
-  };
-  return (
-    <ModalWrapper open={props.open}>
-      <ModalHeader>
-        <ModalRemoveButton onClick={handleRemoveModal}>x</ModalRemoveButton>
-      </ModalHeader>
-      <ModalBody>{props.children}</ModalBody>
-      <ModalFooter>{props.footer}</ModalFooter>
-    </ModalWrapper>
-  );
-};
+class Modal extends Component {
+  handleRemoveModal() {
+    this.props.closeModal();
+  }
+
+  render() {
+    return (
+      <ModalWrapper open={this.props.open}>
+        <ModalHeader>
+          <ModalRemoveButton
+            className="removeButton"
+            onClick={this.handleRemoveModal}
+          >
+            x
+          </ModalRemoveButton>
+        </ModalHeader>
+        <ModalBody>{this.props.children}</ModalBody>
+        <ModalFooter className="footer">{this.props.footer}</ModalFooter>
+      </ModalWrapper>
+    );
+  }
+}
 
 Modal.propTypes = {
   closeModal: PropTypes.func.isRequired,
