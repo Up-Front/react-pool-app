9b5ae356851735edbee9cff122386e06d05af684
Steven Straatemans <steven@straatemans.nl>
2018-03-19 20:38:11 +0100

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Authorization from './components/shared/components/Authorization';
 import Dashboard from './components/Dashboard';
@@ -13,28 +13,24 @@ import configureStore from './store';
 const initialState = window.__INITIAL_STATE__; // set initial state here
 const store = configureStore(initialState);
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <Router>
-            <Template>
-              <React.Fragment>
-                <Route exact path="/" component={Authorization(Dashboard)} />
-                <Route exact path="/login" component={Login} />
-                <Route
-                  exact
-                  path="/leaderboard"
-                  component={Authorization(Leaderboard)}
-                />
-              </React.Fragment>
-            </Template>
-          </Router>
-        </Provider>
-      </ThemeProvider>
-    );
-  }
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <Template>
+          <React.Fragment>
+            <Route exact path="/" component={Authorization(Dashboard)} />
+            <Route exact path="/login" component={Login} />
+            <Route
+              exact
+              path="/leaderboard"
+              component={Authorization(Leaderboard)}
+            />
+          </React.Fragment>
+        </Template>
+      </Router>
+    </Provider>
+  </ThemeProvider>
+);
 
 export default App;
